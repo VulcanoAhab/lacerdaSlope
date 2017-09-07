9ac4a044a34b5f1c1bad64883502c2d121976867
VulcanoAhab <vulcano.ahab@gmail.com>
2017-09-07 17:20:59 -0300

    Adding OLX horseman crawler

--- olx/response_search.js
+++ olx/response_search.js
@@ -4,95 +4,20 @@ var lacerda=require('../lacerda/es_utils.js');
 var db=require('./persistance.js')
 var esUtils=lacerda.esUtils;
 
-var build_payments=function(response){
-  var payments=[];
-  if (response.accepts_mercadopago){
-    payments.push('MercadoPago');
-  }
-  if (response.non_mercado_pago_payment_methods){
-    var pays=response.non_mercado_pago_payment_methods;
-    for (i=0; i<pays.length; i++) {
-      var p=pays[i];
-      payments.push(p);
-    }
-  }
-  return payments;
-}
-
-var build_descriptions=function(response){
-  var dis=[];
-  if (response.descriptions){
-    var ds=response.descriptions;
-    for (i=0;i<ds.length;i++){
-      var d=ds[i];
-      dis.push(d.id);
-    }
-  }
-  return dis;
-}
-
-
 var response = function () {
 
   this.results=[];
-
   this.metadata={};
+  this.lastUrl="";
+  this.Country="";
 
-  this.parse=function(raw_response){
-
-    var _response=JSON.parse(raw_response);
-    var _search=_response.results;
-
-    var offset=_response.paging.offset;
-    var limit=_response.paging.limit;
-
-    var item_count=_response.paging.total;
-    var search_results=[];
-
-    console.log('TOTAL', item_count, typeof item_count);
-
-    if (item_count>0) {
-      for (i=0; i<_search.length; i++){
-
-        var _resp=_search[i];
-        var seller_addr=_resp.seller_address;
-
-        var resp={
-          "item_id":_resp.id,
-          "title":_resp.title,
-          "subtitle":_resp.subtitle,
-          "title_cloud":esUtils.textToWordsList(_resp.title),
-          "sold_count":_resp.sold_quantity,
-          "state":seller_addr.state.name,
-          "country":seller_addr.country.name,
-          "city":seller_addr.city.name,
-          "currency":_resp.currency_id,
-          "price":_resp.price,
-          "url":_resp.permalink,
-          "location":seller_addr.country.name,
-          "payment":build_payments(_resp),
-          "descriptions_ids":build_descriptions(_resp)
-        }
-
-        //update values
-        resp.search_term=this.metadata.search_term;
-        resp.search_created_at=this.metadata.search_created_at;
-        resp.description='toDo';
-        resp.page=offset/limit;
-
-        this.metadata.offset=offset;
-        this.metadata.limit=limit;
-        this.metadata.item_count=item_count;
-
-        //keep in results
-        this.results.push(resp);
-      }}
-    }
+  this.extract=function(htmlDocument) {
+  }
 
   this.insert=function () {
       this.results.map(function(e){db.mercadoES.insert_doc(e);})
   }
 
 }
-
+//
 exports.response=response;
--- olx/search.js
+++ olx/search.js
@@ -1,14 +1,100 @@
 var exports=module.exports;
 
+// == imports
 var Horseman = require('node-horseman');
 var fs = require('fs');
-
-var emarket=require('./response_search.js')
 var mconfigs=require('./configs.json');
+var db=require('./persistance.js')
 var esLacerda=require('../lacerda/es_utils.js');
 var esUtils=esLacerda.esUtils;
 
+
+// ============== helper functions ====== ugly, will be improved
+var Helpers = {
+
+  extractPageNumber: function (urlIn){
+    var re=/o=(\d+)/i;
+    var stringNumber=urlIn.match(re);
+    if (!stringNumber.length){return 0}
+    var number=Number(stringNumber[1]);
+    if (!number){return 0;}
+    return number
+  },
+
+  extractPrice: function (priceRawString) {
+    if (!priceRawString){return 0;}
+    var elclean=priceRawString.replace("R$","");
+    if (!elclean){return 0}
+    var price=Number(elclean.trim());
+    if (!price){return 0;}
+    return price
+  },
+
+  extractRegion: function (stringAdrr) {
+    var baseDict={"city":"notDetected","state":"notDetected"};
+    var listCityState=stringAdrr.split("-");
+    if (!listCityState.length){return baseDict;}
+    if (listCityState[0]){
+      baseDict["city"]=listCityState[0].trim("\t").trim();
+    }
+    if (listCityState[1]){
+      baseDict["state"]=listCityState[1].trim("\t").trim();
+    }
+    return baseDict;
+  },
+
+  extractDateTime: function (datisDict) {
+    var monthDict={
+      "Jan":1,
+      "Fev":2,
+      "Mar":3,
+      "Abr":4,
+      "Jun":5,
+      "Jul":6,
+      "Ago":8,
+      "Set":9,
+      "Out":10,
+      "Nov":11,
+      "Dez":12,
+
+    }
+    var today=new Date()
+    var elDate=datisDict.date;
+    if (!elDate){return null}
+    //do date
+    if (elDate.toLowerCase().trim() == "hoje"){
+      var day=today.getDate();
+      var month=today.getMonth();
+    } else {
+      var dM=elDate.split(" ");
+      var elMonth=dM[1].trim();
+      var day=dM[0].trim();
+      var month=monthDict[elMonth];
+    }
+    //couldn't go back a year yet - dont know how they display with year
+    var year=today.getFullYear();
+    // do time
+    var inTime=datisDict.time;
+    if (!inTime){
+      hour=0
+      minute=0
+    }
+    var times=inTime.split(":");
+    if (!times.length){
+      var hour=0;
+      var minute=0;
+    } else {
+      var hour=times[0].trim();
+      var minute=times[1].trim();
+    }
+    return new Date(year, month, day, hour, minute);
+  }
+
+}
+// ============== helper functions ======
+
 var oSearch = function () {
+  //search globals
   this.urls_list=[];
   this.configs=mconfigs;
   this.bases={
@@ -19,80 +105,153 @@ var oSearch = function () {
       return _url
     }
   };
-
-  this.mount_url=function(keyword, country_name, pageNumber) {
-
+  //mount requests
+  this.mount_request=function(keyword, country_name, pageNumber) {
     _buildUrl=this.bases[country_name.toLowerCase()];
     _url=_buildUrl(keyword, pageNumber)
-
     return {"url":_url,
             "search_term":keyword,
             "country":country_name,
             "pageNumber":pageNumber}
   };
 
-  // this.add_url=function(keyword, country_id, category_id, offset){
-  //   var url=this.mount_url(keyword, country_id, category_id, offset);
-  //   this.urls_list.push(url);
-  // }
-  //
-  // this.mount_urls=function () {
-  //   var ilimit=mconfigs.keywords.length;
-  //   //var ilimit=2;
-  //   for (i=0; i<ilimit; i++){
-  //     var keyword=mconfigs.keywords[i];
-  //     var jlimit=mconfigs.countries.length;
-  //     //var jlimit=2;
-  //     for (j=0; j<jlimit;j++){
-  //       var country=mconfigs.countries[j];
-  //       climit=mconfigs.categories.length
-  //       for (c=0; c<climit; c++){
-  //         var category=mconfigs.categories[c];
-  //         this.add_url(keyword, country, category, 0);
-  //       }
-  //     }
-  //   }
-  // }
+  this.add_url=function(keyword, country_id, pageNumber){
+    var url=this.mount_url(keyword, country_id, pageNumber);
+    this.urls_list.push(url);
+  }
+
+  this.mountBaseUrls=function () {
+    var ilimit=mconfigs.keywords.length;
+    //var ilimit=2;
+    for (i=0; i<ilimit; i++){
+      var keyword=mconfigs.keywords[i];
+      var jlimit=mconfigs.countries.length;
+      //var jlimit=2;
+      for (j=0; j<jlimit;j++){
+        var country=mconfigs.countries[j];
+          this.add_url(keyword, country, 1);
+      }
+    }
+  }
 
 
-  this.search=function(url) {
+  this.search=function(requestDict) {
+    //init var
     var that=this;
     var horseman = new Horseman();
+    var datis=new Date(new Date().getTime()).toString();
+    var _url=requestDict.url;
+    // start response obj
+    rObj={"metadata":{}, "results":[], "error":{}};
+    rObj.metadata.search_term=requestDict.search_term;
+    rObj.metadata.search_created_at=datis;
+    rObj.metadata.country=requestDict.country;
+    rObj.metadata.page=Helpers.extractPageNumber(_url);
 
+    // start data fetch
+    console.log("[+] GETTING: ", _url)
     horseman
       .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
-      .open(url)
+      .open(_url)
+      .waitForSelector('a[title="Última página"]')
+      .evaluate(function(resp) {
+        try {
+          //Page-Global values
+          var lastis=$('a[title="Última página"]');
+          if (lastis){
+            resp.metadata.lastUrl=lastis[0].href;
+            } else {
+            resp.metadata.lastUrl="last";
+          }
+          var elements=document.getElementsByClassName('OLXad-list-link');
+          var item_count=elements.length
 
-      //=== persist html file ===
-      // .html()
-      // .then(function(htmlPage){
-      //   fs.writeFile('test.html', htmlPage, function (err) {
-      //     if (err) throw err;
-      //     console.log('Saved!');
-      //   });
-      // })
+          if (item_count>0) {
+            //grabbers functions
+            function grabPrice(el) {
+              var elnObj=el.getElementsByClassName("OLXad-list-price");
+              if (!elnObj.length){return "";}
+              var eln=elnObj[0].textContent;
+              return eln
+            }
 
-      .log()
+            function grabRegion(el){
+              var elAdrrObj=el.getElementsByClassName("text detail-region");
+              if (!elAdrrObj.length) {return "";}
+              var elAdrr=elAdrrObj[0].textContent;
+              return elAdrr
+            }
 
-      // .type('input[name="q"]', 'github')
-      // .click('[name="btnK"]')
-      // .keyboardEvent('keypress', 16777221)
-      // .waitForSelector('div.g')
-      // .count('div.g')
-      .close();
+            function grabDateTime(el){
+              baseDict={"time":null, "date":null}
+              var elDateObj=el.getElementsByClassName("text mb5px");
+              if (elDateObj.length){
+                baseDict["date"]=elDateObj[0].textContent
+                baseDict["time"]=elDateObj[1].textContent
+              }
+              return baseDict
+            }
 
+            //Element values
+            for (i=0; i<elements.length; i++){
+              var el=elements[i];
+              var price=grabPrice(el);
+              var region=grabRegion(el);
+              var dateTime=grabDateTime(el);
+              var title=el.getAttribute("title");
+              var targetUrl=el.getAttribute("href");
+              if (!targetUrl.indexOf("olx.com")){continue}
+              resp.results.push({
+                        "id":el.getAttribute("id"),
+                        "url":targetUrl,
+                        "price":price,
+                        "region":region,
+                        "created_at":dateTime,
+                        "title":title})}}
+          return resp;
+        } catch (e) {
+            resp.error=e;
+            return resp;
+          }
+        }, rObj)// end evaluate
+      .then (function(rObj) {
+        rObj.metadata.limit=Helpers.extractPageNumber(rObj.metadata.lastUrl);
+        //map => last tranforms and save
+        rObj.results.map(function(e){
+          //location
+          var region=e["region"];
+          delete e["region"];
+          region=Helpers.extractRegion(region);
+          e.city=region.city
+          e.state=region.state
+          e.country=rObj.metadata.country
+          e.location=rObj.metadata.country
+          //price
+          e.price=Helpers.extractPrice(e.price);
+          //dateTime
+          e.created_at=Helpers.extractDateTime(e.created_at);
+          //wordcloud
+          e.title_cloud=esUtils.textToWordsList(e.title);
+          //db.mercadoES.insert_doc(e)
+        });
 
+        console.log("******************\n", rObj);
+        console.log("************************");
+
+        return horseman.close();
+      });
+  }
 
   // this.search_many=function () {
   //   this.urls_list.map( function(e) {
   // }
   //
-  }
+
 }
 
 
 var olxis=new oSearch();
-var _urlObj=olxis.mount_url("tv box", "Brazil", 1);
-olxis.search(_urlObj.url);
+var _reqObj=olxis.mount_request("tv box", "Brazil", 1);
+olxis.search(_reqObj);
 
 //exports.oSearch=oSearch
